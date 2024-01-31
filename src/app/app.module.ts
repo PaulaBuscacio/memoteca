e711cfcfc952c275b470229acfe722ad39ba9de7
PaulaBuscacio <paula@buscacio.net>
2024-01-30 21:53:44 -0300

    componentes de header, footer e assinatura criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { AssinaturaComponent } from './componentes/assinatura/assinatura.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    AssinaturaComponent
   ],
   imports: [
     BrowserModule
