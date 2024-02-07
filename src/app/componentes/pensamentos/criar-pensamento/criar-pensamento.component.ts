ee8d3a4e7a3a37ece4b703eb5dc7b3e28cf28e38
PaulaBuscacio <paula@buscacio.net>
2024-02-07 07:48:28 -0300

    criacao de formulario reativo com o FormBuilder

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,23 +10,25 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-pensamento = {
 
-  conteudo: '',
-  autoria: '',
-  modelo: ''
-}
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulario reativo'],
+      autoria: ['Angular'],
+      modelo: ['modelo1']
+    })
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() =>{
+    this.service.criar(this.formulario.value).subscribe(() =>{
       this.router.navigate(['/listarPensamento'])
     })
   }
